2ad6865e8924609d97bd25012bf8f43e4388fd10
Anthony Cedeno <anthony.cedeno@upr.edu>
2017-02-08 22:54:20 -0400

    Change the lines for the oval and fill the oval.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,14 @@
 import javax.swing.JFrame;
 
+
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Github Primer");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+		JFrame myFrame = new JFrame("Anthony");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocationRelativeTo(null);
+		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+		myFrame.getContentPane().add(myPanel);
+        myFrame.setVisible(true);
 	}
 }
